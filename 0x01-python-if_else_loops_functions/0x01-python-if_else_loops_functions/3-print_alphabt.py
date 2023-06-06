543d78e43a70479b4096d7f3e24f7f683f53982a
FRANCIS <122996379+Hyake@users.noreply.github.com>
2023-06-06 15:16:07 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
 for letter in range(97, 123):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
         print("{}".format(chr(letter)), end="")
